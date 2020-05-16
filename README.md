71f94dc33b81a4ec3f461f87eaa99623ed422d4d
Lars Berntzon <lars.berntzon@cecilia-data.se>
2020-05-16 19:21:36 +0200

    Hopefully correct colors: GRBG

--- README.md
+++ README.md
@@ -17,4 +17,7 @@ When getting a raw image (jpeg+raw), the first part of the file is a normal jpeg
 comes a 32768 bytes header starting with the text "BRCMo".
 The raw image then follows which 18678272 bytes with rows 6112 bytes long: 6084 bytes (4056*1.5!!) of pixels (groups of 3),  12 blanking?? and 16 image related data.
 In total 3056 rows with the last 16 rows containing other data (even in the last 28 bytes of the rows).
-Maybe the pixels are is ordered in groups of [2 x RGB LSB + 1 x RGB MSB]
+
+Pixels are ordered by | Gm | Rm | Gl+Rl |
+                      | Bm | Gm | Bl+Gl |
+m : MSB 8 bits, l = LSB 4 bits
--- imx477/extract_raw.py
+++ imx477/extract_raw.py
@@ -24,8 +24,7 @@ def process_simple_channel_split(row, fout):
         for ch in range(3):
             fout[ch].write(bytes([row[p + ch]]))
 
-
-def process_hyp2(row, fout):
+def process_row(row, fout):
     '''
      format hypotesis:
      Input rows are ordered as:
@@ -46,57 +45,20 @@ def process_hyp2(row, fout):
         The raw file row length seems to rime with this to give correct pixel width.
         6112 (-28) * 2/3 = 4056
     '''
-    if not row:
-        # End of file.
-        for ch in range(4):
-            fout[ch].truncate(3040 * 4056 * 2 / 3)
+    channels = [0, 1] if odd_row else [2, 3]
+    for p in range(0, BRCMo.pixel_row_size, 3):
+        # Write out per channel in little endian 16 bit
+        v1 = row[p] + ((row[p+2]&0xF)<<8)
+        v2 = row[p+1] + ((row[p+2]&0xF0)<<4)
 
-    else:
-        channels = [0, 1] if odd_row else [2, 3]
-        for p in range(0, BRCMo.pixel_row_size, 3):
-            # Write out per channel in little endian 16 bit
-            v1 = (row[p]<<4) + (row[p+2]&0xF)
-            v2 = (row[p+1]<<4) + ((row[p+2]>>4)&0xF)
+        write_int16_le(fout[channels[0]], v1)
+        write_int16_le(fout[channels[1]], v2)
 
-            write_int16_le(fout[channels[0]], v1)
-            write_int16_le(fout[channels[1]], v2)
-
-
-def process_hyp1(row, fout):
-    '''
-     format hypotesis:
-     Input rows are ordered as:
-     0   1   2     3   4   5    6       7       8
-     1la 1lb 1lc   2la 2lb 2lc  1ua|2ua 1ub|2ub 1uc|2uc
-           |
-           V
-     16 out (a-channel):
-     0   1   2   3
-     1la 1ua 2la 2ua
-
-     Observations:
-        The raw file row length seems to rime with this to give correct pixel width.
-        6112 (-28) * 2/3 = 4056
-    '''
-    # End of file.
-    if not row:
-        for ch in range(3):
-            fout[ch].truncate(3040 * 4056 * 2 / 3)
-
-    else:
-        for p in range(0, BRCMo.pixel_row_size, 9):
-            for ch in range(3):
-                # Write out per channel in little endian 16 bit
-                fout[ch].write(bytes([row[p + ch], (row[p + ch + 6] & 0xF)]))
-                fout[ch].write(bytes([row[p + 3 + ch], (row[p + ch + 6] & 0xF0) >> 4]))
 
 if __name__ == '__main__':
     stat = os.stat(sys.argv[1])
 
     brcmo_start = stat.st_size - BRCMo.size
-
-    #processor = process_simple_channel_split
-    processor = process_hyp2
 
     with open(sys.argv[1], "rb") as fin:
         fin.seek(brcmo_start, 0)
@@ -106,20 +68,18 @@ if __name__ == '__main__':
             
         fout = [None, None, None, None]
         with open(sys.argv[1] + ".raw", "wb") as raw, \
-             open(sys.argv[1] + ".raw-0", "wb") as fout[0], \
-             open(sys.argv[1] + ".raw-1", "wb") as fout[1], \
-             open(sys.argv[1] + ".raw-2", "wb") as fout[2], \
-             open(sys.argv[1] + ".raw-3", "wb") as fout[3]:
+             open(sys.argv[1] + ".raw-g1", "wb") as fout[0], \
+             open(sys.argv[1] + ".raw-r", "wb") as fout[1], \
+             open(sys.argv[1] + ".raw-b", "wb") as fout[2], \
+             open(sys.argv[1] + ".raw-g2", "wb") as fout[3]:
             while True:
                 row = fin.read(BRCMo.raw_width)
                 raw.write(row[0:BRCMo.pixel_row_size])
                 if not row:
                     break
-                processor(row, fout)
+                process_row(row, fout)
                 odd_row = not odd_row
 
-            # Indicate eof
-            processor(row, fout)
-
-
-        
+            # Remove last 16 rows of data.
+            for ch in range(4):
+                fout[ch].truncate(3040 * 4056 * 2 / 3)
