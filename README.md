fbba71b3147b4e328e75b242d1bccdf29a9adb5f
Lars Berntzon <lars.berntzon@cecilia-data.se>
2020-05-16 14:33:02 +0200

    More findings about the format

--- README.md
+++ README.md
@@ -14,5 +14,6 @@ References
 Notes
 -----
 When getting a raw image (jpeg+raw), the first part of the file is a normal jpeg, in the remaining part (18711040 bytes) then
-comes a 32768 bytes header starting with the text "BRCM".
-The raw image then follows which is rows of 6112 pixes (ordered by 3 bytes each) x 3056 rows each line is divided by 3 fields.
+comes a 32768 bytes header starting with the text "BRCMo".
+The raw image then follows which 18678272 bytes with rows 6112 bytes long: 6084 bytes (4056*1.5!!) of pixels (groups of 3),  12 blanking?? and 16 image related data.
+In total 3056 rows with the last 16 rows containing other data (even in the last 28 bytes of the rows).
--- imx477/extract_raw.py
+++ imx477/extract_raw.py
@@ -5,15 +5,23 @@ import sys
 
 class BRCMo:
     size = 18711040
+    header_size = 32768
     raw_width = 6112
 
 stat = os.stat(sys.argv[1])
 
 brcmo_start = stat.st_size - BRCMo.size
-print(brcmo_start)
 
-with open(sys.argv[1] + ".rgb24", "wb") as fout:
-    with open(sys.argv[1], "rb") as fin:
+with open(sys.argv[1], "rb") as fin:
+    fin.seek(brcmo_start, 0)
+    with open(sys.argv[1] + ".brcm", "wb") as fout:
+        brcm = fin.read(BRCMo.header_size)
+        fout.write(brcm)
+        
+    with open(sys.argv[1] + ".rgb24", "wb") as fout:
         while True:
-            marker = fin.read(2)
-
+            row = fin.read(BRCMo.raw_width)
+            if not row:
+                break
+            #fout.write(row[0:6112-28])
+            fout.write(row[0:6112])
