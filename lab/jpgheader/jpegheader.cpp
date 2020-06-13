85388294a3eb50c94b8e62c995395a6ec8a34bee
tooextlb <lars.berntzon@external.atlascopco.com>
2020-06-13 21:29:23 +0200

    Backup
    Change-Id: I02d2b3327882df7ffe782b14da92131f6c32cd68

--- lab/jpgheader/jpegheader.cpp
+++ lab/jpgheader/jpegheader.cpp
@@ -1,99 +1,103 @@
+#include <stdio.h>
 #include <arpa/inet.h>
 #include <iostream>
 #include <fstream>
+#include <stdexcept>
 
-static int read_marker(std::ifstream &fin)
+class RawStreamReceiver
+{
+public:
+    class Exception : std::runtime_error
+    {
+    public: Exception(char *text) : std::runtime_error(text) {}
+    };
+
+    RawStreamReceiver();
+
+    void accept_byte(uint8_t byte);
+
+private:
+    enum class State {
+        WANT_FF,
+        WANT_TYPE,
+        WANT_S1,
+        WANT_S2,
+        WANT_SKIP,
+        ENTROPY_WANT_SKIP,
+        ENTROPY_DATA_WANT_S1,
+        ENTROPY_DATA_WANT_S1,
+        WANT_ENTROPY_DATA,
+        END_OF_JPEG,
+        INVALID} state {State::WANT_FF};
+    int pos {0};
+    int s1 {0}; // Length field, first byte MSB.
+    int s2 {0}; // Length field, second byte LSB.
+    int skip_bytes {0}; //Counter for skipping bytes.
+};
+
+/**
+ * @brief RawStreamReceiver::accept_byte Accepts next byte of input and sets state accordingly.
+ * @param byte Next byte to handle.
+ * @return
+ */
+void RawStreamReceiver::accept_byte(uint8_t byte)
 {
     uint8_t ff = 0;
     uint8_t type = 0;
     uint16_t len;
     static int saved_type = -1;
 
-    int pos = fin.tellg();
-
-    if (saved_type == -1)  {
-        if(!fin.read(reinterpret_cast<char *>(&ff), 1)) {
-            return 0;
-        }
-
-        if (ff != 0xff) {
-            std::cerr << "Expected 0xFF but got " << (int)ff << " at segment starting at " << std::hex << pos << std::endl;
-            return 0;
-        }
-
-        if(!fin.read(reinterpret_cast<char *>(&type), 1)) {
-            return 0;
-        }
-    }
-    else {
-        pos -= 2; // The FF for this type was actually found during the entropy_data scan.
-        type = saved_type;
-        saved_type = -1;
-    }
-
-    std::cout << "Found type: " << std::hex << (int)type << " at segment starting at " << std::hex << pos << std::endl;
-    bool read_entropy_data = false;     // Some segmens are followed by entropy data. FF are escaped as FF 00.
-    switch(type)
+    switch(state)
     {
-    case 0xd8: // SOI (Start of image)
-        return 1;
+    case State::END_OF_JPEG:
+    case State::INVALID:
+        throw Exception("State invalid");
+        return;
 
-    case 0xd9: // EOI (End of image)
-        return 0;
-
-    case 0xda: // SOS (Start of scan)
-        read_entropy_data = true;
-        // Fall through
-
-    case 0xe0: // JFIF APP0
-    case 0xe1: // JFIF APP0
-    case 0xdb: // Quantization Table
-    case 0xc0: // Baseline DCT
-    case 0xc4: // Huffman Table
-        if (!fin.read(reinterpret_cast<char *>(&len), 2)) {
-            return 0;
+    case State::WANT_FF:
+        if (byte != 0xFF) {
+            throw Exception("Expected 0xFF");
         }
-        len = ntohs(len);
-        std::cout << "Len:" << std::dec << len << std::endl;
-        len -= 2;
-        fin.seekg((unsigned)fin.tellg() + len);
+        state = State::WANT_TYPE;
+        return;
 
-        if (read_entropy_data) {
-            // Scan pass the entropy_data section.
-            // Handle special case for 0xFF:
-            // 0xFF 0x00 is an escaped FF for the section.
-            // 0xFF 0xFF is padding.
-            // 0xFF <any> means a new segment of 0xFF <type>
-            enum { NORMAL, FOUND_FF } state = NORMAL;
-            while(fin)
-            {
-                if (!fin.read(reinterpret_cast<char *>(&ff), 1)) {
-                    return 0;
-                }
-                switch(state)
-                {
-                case NORMAL:
-                    if (ff == 0xFF) {
-                        state = FOUND_FF;
-                    }
-                    continue;
+    case State::WANT_ENTROPY_DATA:
+        if (byte == 0xFF) {
+            state = State:
 
-                case FOUND_FF:
-                    if (ff == 0) {
-                        state = NORMAL;
-                    }
-                    else if (ff == 0xFF) {
-                        state = FOUND_FF;
-                    }
-                    else {
-                        saved_type = ff;
-                        return 1;
-                    }
-                }
-            }
+        }
+        return;
+
+    case State::WANT_TYPE:
+        switch(byte)
+        {
+        case 0xd8: // SOI (Start of image)
+            state = State::WANT_FF;
+            return;
+
+        case 0xd9: // EOI (End of image)
+            state = State::END_OF_JPEG;
+            return;
+
+        case 0xda: // SOS (Start of scan)
+        case 0xdb: // Quantization Table
+        case 0xc0: // Baseline DCT
+        case 0xc4: // Huffman Table
+            state = State::ENTROPY_DATA_WANT_S1;
+            return;
+
+        case 0xe0: // JFIF APP0
+        case 0xe1: // JFIF APP0
+            state = State:WANT_S1;
+            return;
         }
 
-        return 1;
+    default:
+        fprintf(stderr, "%s(%s): Unknown internal state.\n", __FILE__, __func__);
+        state = State::INVALID;
+        throw Exception("Internal error, unknown state");
+    }
+
 
     default:
         std::cout << "Unknown type: " << (unsigned int)type << " at segment starting at " << std::hex << pos << std::endl;
